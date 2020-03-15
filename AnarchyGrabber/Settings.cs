dd0a77f21648081185441736e05d957f394691da
iLinked <ilinkedisameme@gmail.com>
2020-03-15 07:35:55 +0100

    bug fixes

--- AnarchyGrabber/Settings.cs
+++ AnarchyGrabber/Settings.cs
@@ -2,6 +2,6 @@
 {
     class Settings
     {
-        public static string Webhook = "https://discordapp.com/api/webhooks/688472587436949521/apm92SSHxCHkzWvSEOQPi69WSkkHjb3VuJhUXGT7T4PpXEsoP9HKG5K8FWTp2FRjOHDr";
+        public static string Webhook = "";
     }
 }
